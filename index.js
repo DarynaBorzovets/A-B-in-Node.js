82799a962db4ff861761c3570e857580ceb6d3b4
DarynaBorzovets <biz.daryna@gmail.com>
2024-08-20 13:16:47 -0400

    Task is done. Daryna

--- index.js
+++ index.js
@@ -9,5 +9,21 @@
 //
 
 // TODO: Actually do the sum here.
-console.log("Not implemented.");
 
+// Get command-line arguments
+const args = process.argv.slice(2);
+
+
+// Convert arguments to numbers
+const num1 = parseFloat(args[0]);
+const num2 = parseFloat(args[1]);
+
+// Check if both arguments are valid numbers
+if (isNaN(num1) || isNaN(num2)) {
+    console.error('Both arguments must be valid numbers.');
+    process.exit(1);
+}
+
+const sum = num1 + num2;
+
+console.log(sum);
